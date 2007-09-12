f0c46710d253d21b7643fb8f18b0f8322612f323
Todd Bryan <todd@rightscale.com>
2007-09-12 23:02:48 +0000

    RightAws release 1.2.0, source changes
    git-svn-id: https://wush.net/svn/rightscale/right_aws/trunk@1719 9f0cbaf6-ce18-0410-ad37-d14a22affa91

--- lib/right_aws.rb
+++ lib/right_aws.rb
@@ -48,8 +48,8 @@ require 'sqs/right_sqs'
 module RightAws #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 1
-    MINOR = 1
-    TINY  = 1
+    MINOR = 2
+    TINY  = 0
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
