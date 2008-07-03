160ceffceadbf4e91cd71ed25d8fcaca07128e6b
Todd Bryan <todd@rightscale.com>
2008-07-03 23:24:51 +0000

    #1381: RightAws 1.7.2 release branch - version to 1.7.2
    git-svn-id: https://wush.net/svn/rightscale/right_aws/release_1_7_2@4533 9f0cbaf6-ce18-0410-ad37-d14a22affa91

--- lib/right_aws.rb
+++ lib/right_aws.rb
@@ -52,8 +52,8 @@ require 'sdb/right_sdb_interface'
 module RightAws #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 1
-    MINOR = 8
-    TINY  = 0
+    MINOR = 7
+    TINY  = 2
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
