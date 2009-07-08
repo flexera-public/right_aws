a150dedfdf9fdc0dbcf304a5c18aa7f1719c59a9
konstantin <konstantin@rightscale.com>
2009-07-08 18:11:49 +0400

    RDS fixes

--- lib/rds/right_rds_interface.rb
+++ lib/rds/right_rds_interface.rb
@@ -23,56 +23,7 @@
 
 module RightAws
 
-  # = RightAws::AcfInterface -- RightScale Amazon's CloudFront interface
-  # The AcfInterface class provides a complete interface to Amazon's
-  # CloudFront service.
-  #
-  # For explanations of the semantics of each call, please refer to
-  # Amazon's documentation at
-  # http://developer.amazonwebservices.com/connect/kbcategory.jspa?categoryID=211
-  #
-  # Example:
-  #
-  #  acf = RightAws::AcfInterface.new('1E3GDYEOGFJPIT7XXXXXX','hgTHt68JY07JKUY08ftHYtERkjgtfERn57XXXXXX')
-  #
-  #  list = acf.list_distributions #=>
-  #    [{:status             => "Deployed",
-  #      :domain_name        => "d74zzrxmpmygb.6hops.net",
-  #      :aws_id             => "E4U91HCJHGXVC",
-  #      :origin             => "my-bucket.s3.amazonaws.com",
-  #      :cnames             => ["x1.my-awesome-site.net", "x1.my-awesome-site.net"]
-  #      :comment            => "My comments",
-  #      :last_modified_time => Wed Sep 10 17:00:04 UTC 2008 }, ..., {...} ]
-  #
-  #  distibution = list.first
-  #
-  #  info = acf.get_distribution(distibution[:aws_id]) #=>
-  #    {:enabled            => true,
-  #     :caller_reference   => "200809102100536497863003",
-  #     :e_tag              => "E39OHHU1ON65SI",
-  #     :status             => "Deployed",
-  #     :domain_name        => "d3dxv71tbbt6cd.6hops.net",
-  #     :cnames             => ["web1.my-awesome-site.net", "web2.my-awesome-site.net"]
-  #     :aws_id             => "E2REJM3VUN5RSI",
-  #     :comment            => "Woo-Hoo!",
-  #     :origin             => "my-bucket.s3.amazonaws.com",
-  #     :last_modified_time => Wed Sep 10 17:00:54 UTC 2008 }
-  #
-  #  config = acf.get_distribution_config(distibution[:aws_id]) #=>
-  #    {:enabled          => true,
-  #     :caller_reference => "200809102100536497863003",
-  #     :e_tag            => "E39OHHU1ON65SI",
-  #     :cnames           => ["web1.my-awesome-site.net", "web2.my-awesome-site.net"]
-  #     :comment          => "Woo-Hoo!",
-  #     :origin           => "my-bucket.s3.amazonaws.com"}
-  #
-  #  config[:comment] = 'Olah-lah!'
-  #  config[:enabled] = false
-  #  config[:cnames] << "web3.my-awesome-site.net"
-  #
-  #  acf.set_distribution_config(distibution[:aws_id], config) #=> true
-  #
-  class RdsInterface < RightAwsBase
+    class RdsInterface < RightAwsBase
     
     include RightAwsBaseInterface
 
@@ -100,8 +51,8 @@ module RightAws
     # * <tt>:multi_thread</tt>: true=HTTP connection per thread, false=per process
     # * <tt>:logger</tt>: for log messages, default: RAILS_DEFAULT_LOGGER else STDOUT
     #
-    #  acf = RightAws::AcfInterface.new('1E3GDYEOGFJPIT7XXXXXX','hgTHt68JY07JKUY08ftHYtERkjgtfERn57XXXXXX',
-    #    {:logger => Logger.new('/tmp/x.log')}) #=>  #<RightAws::AcfInterface::0xb7b3c30c>
+    #  rds = RightAws::RdsInterface.new('1E3GDYEOGFJPIT7XXXXXX','hgTHt68JY07JKUY08ftHYtERkjgtfERn57XXXXXX',
+    #    {:logger => Logger.new('/tmp/x.log')}) #=> #<RightAws::RdsInterface::0xb7b3c30c>
     #
     def initialize(aws_access_key_id=nil, aws_secret_access_key=nil, params={})
       init({ :name                => 'RDS',
@@ -246,6 +197,48 @@ module RightAws
       result
     end
 
+    # Create a database security group so that ingress to an RDS Instance can be controlled.
+    # A new security group cannot have the same name as an existing group.
+    #
+    def create_db_security_group(db_security_group_name, db_security_group_description)
+      link = generate_request('CreateDBSecurityGroup', 'DBSecurityGroupName'        => db_security_group_name,
+                                                       'DBSecurityGroupDescription' => db_security_group_description)
+      request_info(link, DescribeDbSecurityGroupsParcer.new(:logger => @logger))[:db_security_groups].first
+    end
+
+    def modify_db_security_group_ingress(action, db_security_group_name, params={}) # :nodoc:
+      request_hash = { 'DBSecurityGroupName' => db_security_group_name}
+      request_hash['CIDRIP']                  = params[:cidrip]                   unless params[:cidrip].blank?
+      request_hash['EC2SecurityGroupName']    = params[:ec2_security_group_name]  unless params[:cidrip].blank?
+      request_hash['EC2SecurityGroupOwnerId'] = params[:ec2_security_group_owner] unless params[:cidrip].blank?
+      link = generate_request(action, request_hash)
+      request_info(link, DescribeDbSecurityGroupsParcer.new(:logger => @logger))[:db_security_groups].first
+    end
+
+    # params:
+    #  :cidrip or (:ec2_security_group_name and :ec2_security_group_owner),
+    def authorize_db_security_group_ingress(db_security_group_name, params={})
+      modify_db_security_group_ingress('AuthorizeDBSecurityGroupIngress', db_security_group_name, params)
+    end
+    
+    # params:
+    #  :cidrip or (:ec2_security_group_name and :ec2_security_group_owner),
+    def authorize_db_security_group_ingress(db_security_group_name, params={})
+      modify_db_security_group_ingress('RevokeDBSecurityGroupIngress', db_security_group_name, params)
+    end
+
+    # Delete a database security group. Database security group must not be associated with any
+    # RDS Instances.
+    #
+    def delete_db_security_group(db_security_group_name)
+      link = generate_request('DeleteDBSecurityGroup', 'DBSecurityGroupName' => db_security_group_name)
+      request_info(link, RightHttp2xxParser.new(:logger => @logger))
+    end
+
+    # --------------------------------------------
+    #  DB Snapshots
+    # --------------------------------------------
+
     # params:
     #  :snapshot =>
     #  :instance =>
@@ -261,6 +254,40 @@ module RightAws
       result
     end
 
+    # Create a DBSnapshot. The source DBInstance must be in Available state
+    #
+    def create_db_snapshot(aws_id, instance_aws_id)
+      link = generate_request('DeleteDBSnapshot', 'TargetDBSnapshotIdentifier' => aws_id,
+                                                  'SourceDBInstanceIdentifier' => instance_aws_id)
+      request_info(link, DescribeDbSnapshotsParcer.new(:logger => @logger))[:db_snapshots].first
+    end
+
+    # Create a new RDS instance from a DBSnapshot. The source DBSnapshot must be
+    # in the "Available" state. The new RDS instance is created with the Default security group.
+    #
+    # Params:
+    #  :instance_class, endpoint_port, :availability_zone
+    #
+    def restore_db_instance_from_db_snapshot(aws_id, instance_aws_id, params={})
+      request_hash = { 'DBSnapshotIdentifier' => aws_id,
+                       'DBInstanceIdentifier' => instance_aws_id }
+      request_hash['DBInstanceClass']  = params[:instance_class]    unless params[:instance_class].blank?
+      request_hash['EndpointPort']     = params[:endpoint_port]     unless params[:endpoint_port].blank?
+      request_hash['AvailabilityZone'] = params[:availability_zone] unless params[:availability_zone].blank?
+      link = generate_request('DeleteDBSnapshot', request_hash)
+      request_info(link, DescribeDbInstancesParcer.new(:logger => @logger))[:db_instances].first
+    end
+
+    # Delete a DBSnapshot. The DBSnapshot must be in the Available state to be deleted.
+    def delete_db_snapshot(aws_id)
+      link = generate_request('DeleteDBSnapshot', 'DBSnapshotIdentifier' => aws_id)
+      request_info(link, DescribeDbSnapshotsParcer.new(:logger => @logger))[:db_snapshots].first
+    end
+
+    # --------------------------------------------
+    #  DB Events
+    # --------------------------------------------
+
     # params:
     #  :duration =>
     def describe_events(params={}, &block)
@@ -274,131 +301,9 @@ module RightAws
       result
     end
 
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-=begin
-
-
-    # Create a new distribution.
-    # Returns the just created distribution or RightAws::AwsError exception.
-    #
-    #  acf.create_distribution('my-bucket.s3.amazonaws.com', 'Woo-Hoo!', true, ['web1.my-awesome-site.net'],
-    #                          { :prefix=>"log/", :bucket=>"my-logs.s3.amazonaws.com" } ) #=>
-    #    {:comment            => "Woo-Hoo!",
-    #     :enabled            => true,
-    #     :location           => "https://cloudfront.amazonaws.com/2008-06-30/distribution/E2REJM3VUN5RSI",
-    #     :status             => "InProgress",
-    #     :aws_id             => "E2REJM3VUN5RSI",
-    #     :domain_name        => "d3dxv71tbbt6cd.6hops.net",
-    #     :origin             => "my-bucket.s3.amazonaws.com",
-    #     :cnames             => ["web1.my-awesome-site.net"],
-    #     :logging            => { :prefix => "log/",
-    #                              :bucket => "my-logs.s3.amazonaws.com"},
-    #     :last_modified_time => Wed Sep 10 17:00:54 UTC 2008,
-    #     :caller_reference   => "200809102100536497863003"}
-    #
-    def create_distribution(origin, comment='', enabled=true, cnames=[], caller_reference=nil, logging={})
-      config = { :origin  => origin,
-                 :comment => comment,
-                 :enabled => enabled,
-                 :cnames  => cnames.to_a,
-                 :caller_reference => caller_reference }
-      config[:logging] = logging unless logging.blank?
-      create_distribution_by_config(config)
-    end
-
-    def create_distribution_by_config(config)
-      config[:caller_reference] ||= generate_call_reference
-      link = generate_request('POST', 'distribution', {}, config_to_xml(config))
-      merge_headers(request_info(link, AcfDistributionListParser.new(:logger => @logger))[:distributions].first)
-    end
-
-    # Get a distribution's information.
-    # Returns a distribution's information or RightAws::AwsError exception.
-    #
-    #  acf.get_distribution('E2REJM3VUN5RSI') #=>
-    #    {:enabled            => true,
-    #     :caller_reference   => "200809102100536497863003",
-    #     :e_tag              => "E39OHHU1ON65SI",
-    #     :status             => "Deployed",
-    #     :domain_name        => "d3dxv71tbbt6cd.6hops.net",
-    #     :cnames             => ["web1.my-awesome-site.net", "web2.my-awesome-site.net"]
-    #     :aws_id             => "E2REJM3VUN5RSI",
-    #     :comment            => "Woo-Hoo!",
-    #     :origin             => "my-bucket.s3.amazonaws.com",
-    #     :last_modified_time => Wed Sep 10 17:00:54 UTC 2008 }
-    #
-    def get_distribution(aws_id)
-      link = generate_request('GET', "distribution/#{aws_id}")
-      merge_headers(request_info(link, AcfDistributionListParser.new(:logger => @logger))[:distributions].first)
-    end
-
-    # Get a distribution's configuration.
-    # Returns a distribution's configuration or RightAws::AwsError exception.
-    #
-    #  acf.get_distribution_config('E2REJM3VUN5RSI') #=>
-    #    {:enabled          => true,
-    #     :caller_reference => "200809102100536497863003",
-    #     :e_tag            => "E39OHHU1ON65SI",
-    #     :cnames           => ["web1.my-awesome-site.net", "web2.my-awesome-site.net"]
-    #     :comment          => "Woo-Hoo!",
-    #     :origin           => "my-bucket.s3.amazonaws.com"}
-    #
-    def get_distribution_config(aws_id)
-      link = generate_request('GET', "distribution/#{aws_id}/config")
-      merge_headers(request_info(link, AcfDistributionListParser.new(:logger => @logger))[:distributions].first)
-    end
-
-    # Set a distribution's configuration 
-    # (the :origin and the :caller_reference cannot be changed).
-    # Returns +true+ on success or RightAws::AwsError exception.
-    #
-    #  config = acf.get_distribution_config('E2REJM3VUN5RSI') #=>
-    #    {:enabled          => true,
-    #     :caller_reference => "200809102100536497863003",
-    #     :e_tag            => "E39OHHU1ON65SI",
-    #     :cnames           => ["web1.my-awesome-site.net", "web2.my-awesome-site.net"]
-    #     :comment          => "Woo-Hoo!",
-    #     :origin           => "my-bucket.s3.amazonaws.com"}
-    #  config[:comment] = 'Olah-lah!'
-    #  config[:enabled] = false
-    #  acf.set_distribution_config('E2REJM3VUN5RSI', config) #=> true
-    #
-    def set_distribution_config(aws_id, config)
-      link = generate_request('PUT', "distribution/#{aws_id}/config", {}, config_to_xml(config),
-                                     'If-Match' => config[:e_tag])
-      request_info(link, RightHttp2xxParser.new(:logger => @logger))
-    end
-
-    # Delete a distribution. The enabled distribution cannot be deleted.
-    # Returns +true+ on success or RightAws::AwsError exception.
-    #
-    #  acf.delete_distribution('E2REJM3VUN5RSI', 'E39OHHU1ON65SI') #=> true
-    #
-    def delete_distribution(aws_id, e_tag)
-      link = generate_request('DELETE', "distribution/#{aws_id}", {}, nil,
-                                        'If-Match' => e_tag)
-      request_info(link, RightHttp2xxParser.new(:logger => @logger))
-    end
-
-    #-----------------------------------------------------------------
-    #      PARSERS:
-    #-----------------------------------------------------------------
-=end
+    # --------------------------------------------
+    #  Parcers
+    # --------------------------------------------
 
     # --------------------------------------------
     #  DB Instances
@@ -410,7 +315,7 @@ module RightAws
       end
       def tagstart(name, attributes)
         case name
-          when 'DBInstance', 'CreateDBInstanceResult', 'DeleteDBInstanceResult', 'ModifyDBInstanceResult'
+          when 'DBInstance', 'CreateDBInstanceResult', 'DeleteDBInstanceResult', 'ModifyDBInstanceResult', 'RestoreDBInstanceFromDBSnapshotResult'
             @db_instance = { :db_security_groups => [], :pending_modified_values => {} }
           when 'DBSecurityGroup' then @db_security_group = {}
         end
@@ -456,7 +361,8 @@ module RightAws
       end
       def tagstart(name, attributes)
         case name
-          when 'DBSecurityGroup'  then @db_security_group  = { :ec2_security_groups => [], :ip_ranges => [] }
+          when 'DBSecurityGroup', 'CreateDBSecurityGroupResult', 'AuthorizeDBSecurityGroupIngressResult', 'RevokeDBSecurityGroupIngress'
+            @db_security_group  = { :ec2_security_groups => [], :ip_ranges => [] }
           when 'EC2SecurityGroup' then @ec2_security_group = {}
           when 'IPRange'          then @ip_range = {}
         end
@@ -471,8 +377,8 @@ module RightAws
         when 'DBSecurityGroupName'        then @db_security_group[:name]        = @text
         when 'Status'
           case @xmlpath
-          when "@p/EC2SecurityGroups/EC2SecurityGroup" then @ec2_security_group[:status] = @text
-          when "@p/IPRanges/IPRanges"                  then @ip_range[:status] = @text
+          when /EC2SecurityGroup$/ then @ec2_security_group[:status] = @text
+          when /IPRanges$/         then @ip_range[:status] = @text
           end
         when 'EC2GroupName'            then @ec2_security_group[:name]     = @text
         when 'EC2SecurityGroupOwnerId' then @ec2_security_group[:owner_id] = @text
@@ -494,7 +400,8 @@ module RightAws
       end
       def tagstart(name, attributes)
         case name
-          when 'DBSnapshot' then @db_snapshot = {}
+          when 'DBSnapshot', 'CreateDBSnapshotResult', 'DeleteDBSnapshotResult'
+            @db_snapshot = {}
         end
       end
       def tagend(name)
