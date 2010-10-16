0b9654378be41a2ba88effd1636c8e526f85e5c5
Aaron Suggs <aaron@ktheory.com>
2010-10-16 16:24:11 -0400

    Basic support for resource tags
    Adds methods:
     RightAws::Ec2#describe_tags
     RightAws::Ec2#create_tags
     RightAws::Ec2#delete_tags
    
    P.S. with minor changes by konstantin

--- lib/awsbase/right_awsbase.rb
+++ lib/awsbase/right_awsbase.rb
@@ -518,6 +518,9 @@ module RightAws
     end
 
     # Format array of items into Amazons handy hash ('?' is a place holder):
+    # Options:
+    #   :default => "something"   : Set a value to "something" when it is nil
+    #   :default => :skip_nils    : Skip nil values
     #
     #  amazonize_list('Item', ['a', 'b', 'c']) =>
     #    { 'Item.1' => 'a', 'Item.2' => 'b', 'Item.3' => 'c' }
@@ -541,7 +544,7 @@ module RightAws
     #     "Filter.2.Key"=>"B",
     #     "Filter.2.Value.1"=>"ba",
     #     "Filter.2.Value.2"=>"bb"}
-    def amazonize_list(masks, list) #:nodoc:
+    def amazonize_list(masks, list, options={}) #:nodoc:
       groups = {}
       Array(list).each_with_index do |list_item, i|
         Array(masks).each_with_index do |mask, mask_idx|
@@ -549,8 +552,12 @@ module RightAws
           key.sub!('?', (i+1).to_s)
           value = Array(list_item)[mask_idx]
           if value.is_a?(Array)
-            groups.merge!(amazonize_list(key, value))
+            groups.merge!(amazonize_list(key, value, options))
           else
+            if value.nil?
+              next if options[:default] == :skip_nils
+              value = options[:default]
+            end
             groups[key] = value
           end
         end
