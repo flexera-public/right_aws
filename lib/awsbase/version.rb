b6b01675d4d0cc1aa29523320388a19e632b6048
konstantin <konstantin@rightscale.com>
2012-04-10 22:39:05 +0400

    Release 3.0.4

--- lib/awsbase/version.rb
+++ lib/awsbase/version.rb
@@ -2,7 +2,7 @@ module RightAws #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 3  unless defined?(MAJOR)
     MINOR = 0  unless defined?(MINOR)
-    TINY  = 3  unless defined?(TINY)
+    TINY  = 4  unless defined?(TINY)
 
     STRING = [MAJOR, MINOR, TINY].join('.') unless defined?(STRING)
   end
