b72934f11a5d4e2294484853d76a13f5009c6999
konstantin <konstantin@rightscale.com>
2012-11-05 19:09:07 -0800

    Refs #14356 - Add support for m3.xlarge

--- lib/ec2/right_ec2.rb
+++ lib/ec2/right_ec2.rb
@@ -91,6 +91,7 @@ module RightAws
                                  'm2.xlarge' ,
                                  'm2.2xlarge',
                                  'm2.4xlarge',
+                                 'm3.xlarge' ,
                                  'm3.2xlarge',
                                 'cc1.4xlarge',
                                 'cg1.4xlarge',
