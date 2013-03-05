b311a11c00219b018aa130302a2887b4d23b9497
konstantin <konstantin@rightscale.com>
2013-03-05 14:21:42 -0800

    acu81709 - version bump #2

--- lib/awsbase/version.rb
+++ lib/awsbase/version.rb
@@ -2,7 +2,7 @@ module RightAws #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 3  unless defined?(MAJOR)
     MINOR = 0  unless defined?(MINOR)
-    TINY  = 4  unless defined?(TINY)
+    TINY  = 5  unless defined?(TINY)
 
     STRING = [MAJOR, MINOR, TINY].join('.') unless defined?(STRING)
   end
